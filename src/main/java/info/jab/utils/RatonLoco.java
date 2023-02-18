3cbe73ff4310da860b1dfacf754ecad588e42348
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-02-18 21:35:00 +0100

    Adding better cli support (#12)

--- src/main/java/info/jab/utils/RatonLoco.java
+++ src/main/java/info/jab/utils/RatonLoco.java
@@ -1,4 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS info.picocli:picocli:4.7.1
 
 package info.jab.utils;
 
@@ -7,17 +8,31 @@ import java.awt.Robot;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 import java.util.stream.IntStream;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public final class RatonLoco {
+// @formatter:off
+@Command(
+    name = "Raton Loco",
+    version = "Raton Loco 1.4",
+    description = "Raton loco is a Windows utility to avoid the Screensaver.\n"
+            + "Usecase: you have a computer that you need to be logged "
+            + "but if you don´t interact with it in a period of time, "
+            + "then the Operating System will show a Screensaver. "
+            + "This utility avoid this case.",
+    mixinStandardHelpOptions = true
+)
+// @formatter:on
+public final class RatonLoco implements Runnable {
 
-    private static final String PARAMETER_UNTIL = "UNTIL";
-    private static final int DEFAULT_UNTIL_VALUE = 9 * 60;
-    private static final int DEFAULT_PAUSE = 60 * 1000;
-    private static final String PARAMETER_HELP = "HELP";
+    @Option(names = { "-u", "--until" }, description = "where x is the number of minutes to run")
+    private Integer untilParam = 9 * 60;
+
+    @Option(names = { "-p", "--pause" }, description = "how many minutes until next mouse movement")
+    private Integer pauseParam = 60 * 1000;
 
     private static final String MOUSE_EMOJI = "\uD83D\uDC2D";
     private static final String CHEESE_EMOJI = "\uD83E\uDDC0";
@@ -27,11 +42,53 @@ public final class RatonLoco {
         //Header
         showHeader();
 
-        //Configuration
-        Map<String, Integer> configuration = processConfiguration(args);
+        int exitCode = new CommandLine(new RatonLoco()).execute(args);
+        System.exit(exitCode);
+    }
 
-        //Process
-        process(configuration);
+    @Override
+    public void run() {
+        try {
+            LocalDateTime start = LocalDateTime.now();
+            Robot robot = new Robot();
+            Integer maxX = 400;
+            Integer maxY = 400;
+            Random random = new Random();
+            Integer until = untilParam;
+            Integer pause = pauseParam;
+
+            String mouse = OS.startsWith("Win") ? "mouse" : MOUSE_EMOJI;
+            if (until == 1) {
+                System.out.println("The " + mouse + " will work for " + until + " minute.");
+            } else {
+                System.out.println("The " + mouse + " will work for " + until + " minutes.");
+            }
+            System.out.println();
+
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            System.out.println(dtf.format(now) + " " + (OS.startsWith("Win") ? "" : MOUSE_EMOJI));
+
+            while (true) {
+                robot.mouseMove(random.nextInt(maxX), random.nextInt(maxY));
+                Thread.sleep(pause);
+
+                now = LocalDateTime.now();
+                Duration duration = Duration.between(start, now);
+                if (duration.toMinutes() == until) {
+                    if (OS.startsWith("Win")) {
+                        System.out.println(dtf.format(now) + " The mouse escaped with the cheese");
+                    } else {
+                        System.out.println(dtf.format(now) + " " + MOUSE_EMOJI + " escaped with the " + CHEESE_EMOJI);
+                    }
+                    break;
+                } else {
+                    System.out.println(dtf.format(now) + " " + (OS.startsWith("Win") ? "" : MOUSE_EMOJI));
+                }
+            }
+        } catch (AWTException | InterruptedException ex) {
+            //Empty
+        }
     }
 
     private static void showHeader() {
@@ -40,88 +97,5 @@ public final class RatonLoco {
         System.out.println(mouseLine);
         System.out.println(symbol + " R A T O N - L O C O  " + symbol);
         System.out.println(mouseLine);
-    }
-
-    private static Map<String, Integer> processConfiguration(String[] args) {
-        Map<String, Integer> configuration = new HashMap<>();
-
-        if (args.length > 0) {
-            if (args.length > 1) {
-                System.out.println("The utility only accepts an unique parameter: HELP | UNTIL=x");
-                System.out.println();
-                System.exit(1);
-            }
-            String[] rawParams = args[0].split("=");
-            if (rawParams[0].equals(PARAMETER_UNTIL)) {
-                try {
-                    configuration.put(PARAMETER_UNTIL, Integer.parseInt(rawParams[1]));
-                } catch (NumberFormatException ex) {
-                    System.err.println("Parameter " + PARAMETER_UNTIL + " only accepts a non decimal value");
-                    System.out.println();
-                    System.exit(1);
-                }
-            }
-            if (rawParams[0].equals(PARAMETER_HELP)) {
-                System.out.println("'Raton loco' is a Windows utility to avoid the Screensaver.");
-                System.out.println();
-                System.out.println("Usecase: you have a computer that you need to be logged");
-                System.out.println("         but in few minutes if you don´t interact with it, ");
-                System.out.println("         it enter the Screensaver. This utility avoid this case");
-                System.out.println();
-                System.out.println("Usage:   The utility accepts a parameter UNTIL=x ");
-                System.out.println("         where x is the number of minutes to run");
-                System.out.println();
-                System.exit(0);
-            }
-        }
-
-        //Defaults
-        if (!configuration.containsKey(PARAMETER_UNTIL)) {
-            configuration.put(PARAMETER_UNTIL, DEFAULT_UNTIL_VALUE);
-        }
-
-        //Show values
-        String mouse = OS.startsWith("Win") ? "mouse" : MOUSE_EMOJI;
-        Integer until = configuration.get(PARAMETER_UNTIL);
-        if (until == 1) {
-            System.out.println("The " + mouse + " will work for " + until + " minute.");
-        } else {
-            System.out.println("The " + mouse + " will work for " + until + " minutes.");
-        }
-        System.out.println();
-
-        return configuration;
-    }
-
-    private static void process(Map<String, Integer> configuration) throws AWTException, InterruptedException {
-        LocalDateTime start = LocalDateTime.now();
-        Robot robot = new Robot();
-        Integer maxX = 400;
-        Integer maxY = 400;
-        Random random = new Random();
-        Integer until = configuration.get(PARAMETER_UNTIL);
-        Integer defaultPause = DEFAULT_PAUSE;
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(dtf.format(now) + " " + (OS.startsWith("Win") ? "" : MOUSE_EMOJI));
-
-        while (true) {
-            robot.mouseMove(random.nextInt(maxX), random.nextInt(maxY));
-            Thread.sleep(defaultPause);
-
-            now = LocalDateTime.now();
-            Duration duration = Duration.between(start, now);
-            if (duration.toMinutes() == until) {
-                if (OS.startsWith("Win")) {
-                    System.out.println(dtf.format(now) + " The mouse escaped with the cheese");
-                } else {
-                    System.out.println(dtf.format(now) + " " + MOUSE_EMOJI + " escaped with the " + CHEESE_EMOJI);
-                }
-                break;
-            } else {
-                System.out.println(dtf.format(now) + " " + (OS.startsWith("Win") ? "" : MOUSE_EMOJI));
-            }
-        }
     }
 }
